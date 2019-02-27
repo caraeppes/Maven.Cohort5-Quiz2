fc60f956a6b4ebb1ae612196989e38a533f7d2e8
Cara Eppes <ceppes@udel.edu>
2019-02-27 14:11:51 -0500

    completed part 1

--- src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
@@ -2,7 +2,16 @@ package com.zipcodewilmington.assessment2.part2;
 
 public class ArrayUtility {
     public Integer[] merge(Integer[] array1, Integer[] array2) {
-        return null;
+        Integer[] answer = new Integer[array1.length + array2.length];
+        for(int i = 0; i < array1.length; i++){
+            answer[i] = array1[i];
+        }
+
+        for(int i = 0; i < array2.length; i++){
+            answer[i + array1.length] = array2[i];
+        }
+        return answer;
+
     }
 
     public Integer[] rotate(Integer[] array, Integer index) {
